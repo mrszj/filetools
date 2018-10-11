a087d51326ebc366164e0ca1c2e9edc63c1470a1
mrszj <120037994@qq.com>
2018-10-11 14:45:15 +0800

    aaaaaaaaa

--- filetools/src/sfile.py
+++ filetools/src/sfile.py
@@ -4,8 +4,30 @@ Created on 2018年9月4日
 @author: szj
 '''
 import os
+import sys
 import platform
-
+def get_dir_size(path):
+    list1 = []
+    fileList = os.listdir(path)  # 获取path目录下所有文件
+    for filename in fileList:
+        pathTmp = os.path.join(path,filename)  # 获取path与filename组合后的路径
+        if os.path.isdir(pathTmp):   # 判断是否为目录
+            get_dir_size(pathTmp)        # 是目录就继续递归查找
+        elif os.path.isfile(pathTmp):  # 判断是否为文件
+            filesize = os.path.getsize(pathTmp)  # 如果是文件，则获取相应文件的大小
+            list1.append(filesize)      # 将文件的大小添加到列表
+    return (sum(list1))
+def get_dir(path):
+    list2 = []
+    fileList = os.listdir(path)  # 获取path目录下所有文件
+    for filename in fileList:
+        pathTmp = os.path.join(path,filename)  # 获取path与filename组合后的路径
+        if os.path.isdir(pathTmp):   # 判断是否为目录       # 是目录就继续递归查找
+            list2.append(pathTmp)      # 将文件的大小添加到列表
+    return (list2)
+def get_file_size(filename):
+    filesize = os.path.getsize(filename)  # 如果是文件，则获取相应文件的大小
+    return (filesize)
 def printPath(level, path):
     global allFileNum
     dirList = []
@@ -35,7 +57,10 @@ def printPath(level, path):
 if __name__ == '__main__':
     print(os.name)
     print(os.path.sep)
+    path = sys.argv[1]
+    path_dir = get_dir(path)
+    for dira in path_dir:
+        print(get_dir_size(dira))
 #     printPath(1,'d:\ftp')
 #     print('总文件数=',allFileNum)
-    
-    
+#list1 = [11,33,55,66]
