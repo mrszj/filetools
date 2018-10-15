02ca65e23bd0fad9f936971c77103b2260062da3
mrszj <120037994@qq.com>
2018-10-15 18:14:48 +0800

    修改脚本

--- filetools/src/copydir.py
+++ filetools/src/copydir.py
@@ -4,10 +4,9 @@ Created on 2018年9月4日
 @author: szj
 @version: 1.0.0
 useage:
-D:\ssdftp\stability>python deldir.py D:\ssdftp\iis\ftp1 3370 13312284892 ".SUTMP
-" "d:\ssdftp\stability\deldir.log" 1800
-每隔1800秒删除D:\ssdftp\iis\ftp1目录下，大小为13312284892字节，文件数量为3370个的子文件夹，忽略.SUTMP文件，记录日志
-到d:\ssdftp\stability\deldir.log
+python D:\ssdftp\stability\deldir.py D:\ssdftp\stability\mu D:\ssdftp\iis\ftp1 D:\ssdftp\stability 3000 5
+每隔3000秒拷贝删除D:\ssdftp\stability\mu目录5次到D:\ssdftp\iis\ftp1目录下，传输完成后，ftp1目录下文件数量为空时，删除空目录继续下轮拷贝，记录日志
+到d:\ssdftp\stability\目录，日志文件名称为copydir_日期.log
 '''
 import os
 import shutil
@@ -15,7 +14,6 @@ import sys
 from time import sleep
 from builtins import str
 import datetime
-import string
 
 #获取目录下所有文件夹
 def get_dir(path):
@@ -27,22 +25,26 @@ def get_dir(path):
             list2.append(pathTmp)
     return (list2)
 
-#判断指定文件夹包含文件数和文件夹大小是否与预期的文件数、大小相等，不判断指定后缀名的文件
+#判断指定文件夹下文件数量是否为0
 def getDirNumSize(dirPath,dirfilenum=0,dirsize=0,suffix=""):
     dirfilenum_tmp = 0
     dirsize_tmp = 0
-    for root, dirs, files in os.walk(dirPath):
-        for f in files:
-            if suffix =="" or os.path.splitext(f)[-1] != suffix:
-                dirsize_tmp += os.path.getsize(os.path.join(root, f))
-                dirfilenum_tmp += 1
+    try:
+        for root, dirs, files in os.walk(dirPath):
+            for f in files:
+                if suffix =="" or os.path.splitext(f)[-1] != suffix:
+                    dirsize_tmp += os.path.getsize(os.path.join(root, f))
+                    dirfilenum_tmp += 1
+    except Exception as e:
+        print(e)
     print("==================")
     print("文件夹%s\n实际文件数量:%s\n期望文件数量:%s" %(dirPath,dirfilenum_tmp,dirfilenum))
     if int(dirfilenum) == int(dirfilenum_tmp):
         return True
     else:
         return False
-    
+
+#拷贝srcdir目录到destdir，重命名为newdirname
 def copyDir(srcdir,destdir,newdirname):
     if (not os.path.exists(srcdir)) or (not os.path.exists(destdir)):
         print("目录不存在,脚本退出")
@@ -52,59 +54,69 @@ def copyDir(srcdir,destdir,newdirname):
     except Exception as ex:
         print("拷贝出错:"+str(ex))
         return False
-    print("copy "+ destdir + os.path.sep + str(newdirname) + " 成功 ")
+    print(destdir + os.path.sep + str(newdirname) + " 成功 ")
     return True
+
+#删除指定文件夹下空文件夹
 def delEmpty(destdir):
     list1 = []
     fileList = os.listdir(destdir)
-    for filename in fileList:
-        pathTmp = os.path.join(destdir,filename)
-        if os.path.isdir(pathTmp):
-            list1.append(pathTmp)
-    for d in list1:
+    try:
+        for filename in fileList:
+            pathTmp = os.path.join(destdir,filename)
+            if os.path.isdir(pathTmp):
+                list1.append(pathTmp)
+        for d in list1:
             if (getDirNumSize(d)):
                 print("准备删除 "+d)
                 shutil.rmtree(d)
                 print("删除完毕 "+d)
             else:
                 return (False)
+    except Exception as e:
+        print(e)
+        return (False)
     return (True)
 
+#写日志
+def writeLog(logdir,content):
+    time1 = datetime.datetime.now().strftime('%Y-%m-%d')
+    time2 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    try:
+        with open(logdir + os.path.sep + "copydir_" + time1 + ".log",'a') as f:
+            f.write(str(time2) + " " + str(content)+"\n")
+    except Exception as e:
+        print(e)
 if __name__ == '__main__':
-#     srcdir = sys.argv[1] #待拷贝的文件夹，例如 D:\ssdftp\stability\mu
-#     destdir = sys.argv[2] #目的路径，一般为ftp目录，例如 D:\ssdftp\iis\ftp1
-#     logfile = sys.argv[3]   #日志文件路径，例如 "d:\ssdftp\stability\copydir.log"
-#     sleeptime = sys.argv[4] #每个循环sleep时间（秒），sleep后判断目的路径是否为空，为空删除空文件夹继续下一轮拷贝，不为空sleep 30秒继续判断
-#     dirnum = sys.argv[5]    #每个循环拷贝文件夹数量
-    srcdir = r"f:\f\1"
-    destdir = r"f:\ff"
-    sleeptime = 10
-    dirnum = 3
-    dirprefix = ""
-    logfile = r"f:\copydirlog.txt"
+    srcdir = sys.argv[1] #待拷贝的文件夹，例如 D:\ssdftp\stability\mu
+    destdir = sys.argv[2] #目的路径，一般为ftp目录，例如 D:\ssdftp\iis\ftp1
+    logdir = sys.argv[3]   #日志文件存放路径，例如 "d:\ssdftp\stability"
+    sleeptime = sys.argv[4] #每个循环sleep时间（秒），sleep后判断目的路径是否为空，为空删除空文件夹继续下一轮拷贝，不为空sleep 30秒继续判断
+    dirnum = sys.argv[5]    #每个循环拷贝文件夹数量
+#     srcdir = r"d:\ftp\ftp1\Oracle11g"
+#     destdir = r"d:\ftp\ftp1\1"
+#     sleeptime = 10
+#     dirnum = 2
+#     dirprefix = ""
+#     logdir = r"d:\ftp\ftp1"
     i = 1
-    with open(logfile,'a') as f:
-        time1 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        f.write(str(time1)+"\n")
+    writeLog(logdir, "开始拷贝" + srcdir +"到" + destdir +"目录")
     while i > 0:
         #拷贝文件
-        print(i % dirnum)
-        print("开始本轮拷贝") 
+        print("开始拷贝") 
         r = copyDir(srcdir,destdir,i)
         if r:
-            with open(logfile,'a') as f:
-                f.write(str(i)+"\n")
+            writeLog(logdir, destdir + os.path.sep + str(i))
         else:
             print("拷贝失败")
             sys.exit(1)
-        if i % dirnum == 0 :
+        if i % int(dirnum) == 0 :
             print("本轮拷贝完成")
-            sleep(sleeptime)
+            writeLog(logdir, "本轮拷贝完成")
+            sleep(int(sleeptime))
             #判断目录是否为空,为空删除空文件夹继续下轮拷贝
             while not delEmpty(destdir):
                 print("目录没有传输完成,等待20秒")
                 sleep(30)
-            with open(logfile,'a') as f:
-                time2 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                f.write(str(time1)+"\n")
+            writeLog(logdir, "删除空目录完成")
         i += 1
--- filetools/src/deldir.py
+++ filetools/src/deldir.py
@@ -4,15 +4,16 @@ Created on 2018年9月4日
 @author: szj
 @version: 1.0.0
 useage:
-D:\ssdftp\stability>python deldir.py D:\ssdftp\iis\ftp1 3370 13312284892 ".SUTMP
-" "d:\ssdftp\stability\deldir.log" 1800
+python D:\ssdftp\stability\deldir.py D:\ssdftp\iis\ftp1 11371 19711138012 ".SUTMP" "d:\ssdftp\stability" 1800
 每隔1800秒删除D:\ssdftp\iis\ftp1目录下，大小为13312284892字节，文件数量为3370个的子文件夹，忽略.SUTMP文件，记录日志
-到d:\ssdftp\stability\deldir.log
+到d:\ssdftp\stability\deldir_日期.log
 '''
+import datetime
 import os
 import shutil
 import sys
 from time import sleep
+
 
 #获取目录下所有文件夹
 def get_dir(path):
@@ -28,11 +29,14 @@ def get_dir(path):
 def getDirNumSize(dirPath,dirfilenum=0,dirsize=0,suffix=""):
     dirfilenum_tmp = 0
     dirsize_tmp = 0
-    for root, dirs, files in os.walk(dirPath):
-        for f in files:
-            if suffix =="" or os.path.splitext(f)[-1] != suffix:
-                dirsize_tmp += os.path.getsize(os.path.join(root, f))
-                dirfilenum_tmp += 1
+    try:
+        for root, dirs, files in os.walk(dirPath):
+            for f in files:
+                if suffix =="" or os.path.splitext(f)[-1] != suffix:
+                    dirsize_tmp += os.path.getsize(os.path.join(root, f))
+                    dirfilenum_tmp += 1
+    except Exception as e:
+        print(e)
     print("==================")
     print("文件夹%s\n实际文件数量:%s\n期望文件数量:%s\n实际大小:%s\n期望大小:%s" %(dirPath,dirfilenum_tmp,dirfilenum,dirsize_tmp,dirsize))
     if int(dirfilenum) == int(dirfilenum_tmp) and int(dirsize) == int(dirsize_tmp) :
@@ -40,12 +44,21 @@ def getDirNumSize(dirPath,dirfilenum=0,dirsize=0,suffix=""):
     else:
         return False
 
+#写日志
+def writeLog(logdir,content):
+    time1 = datetime.datetime.now().strftime('%Y-%m-%d')
+    time2 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    try:
+        with open(logdir + os.path.sep + "deldir_" + time1 + ".log",'a') as f:
+            f.write(str(time2) + " " + str(content)+"\n")
+    except Exception as e:
+        print(e)            
 if __name__ == '__main__':
     path = sys.argv[1]  #ftp目录全路径，例如 D:\ssdftp\iis\ftp1
     dirfilenum = sys.argv[2]    #目录下文件夹包含文件数量
     dirsize = sys.argv[3]   #目录大小
     filesuffix = sys.argv[4]    #忽略的后缀名，例如 .SUTMP
-    logfile = sys.argv[5]   #日志文件路径，例如 "d:\ssdftp\stability\deldir.log"
+    logdir = sys.argv[5]   #日志文件路径，例如 "d:\ssdftp\stability"
     sleeptime = sys.argv[6] #每个循环sleep时间
 #     path = r"D:\ftp\ftp1"
 #     dirfilenum = 4
@@ -60,10 +73,9 @@ if __name__ == '__main__':
                     print("准备删除 "+d)
                     shutil.rmtree(d)
                     print("删除完毕 "+d)
-                    with open(logfile,'a') as f:
-                        f.write(d+"\n")
+                    writeLog(logdir, d)
                     print("写入日志成功")
                 except Exception as e:
                     print(e)
         print("SLEEPING %s 秒" % sleeptime)
-        sleep(sleeptime)
+        sleep(int(sleeptime))
